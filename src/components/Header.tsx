f3fb6ed7229a1b0629b73773ba7cd3cb3e611744
meo2326cc <cccmsp1@gmail.com>
2025-05-02 00:35:48 +0800

    add news page and change navbar link news.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,8 +13,8 @@ export default function Header() {
           <nav>
             <ul className="flex space-x-6">
               <li>
-                <Link href="/" className="text-gray-600 hover:text-gray-900">
-                  首頁
+                <Link href="/news" className="text-gray-600 hover:text-gray-900">
+                  最新消息
                 </Link>
               </li>
               <li>
