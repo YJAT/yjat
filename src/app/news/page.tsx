bc2a8cb4277eb571d6852d85a3326c0f73d44d7b
meo2326cc <cccmsp1@gmail.com>
2025-05-03 20:02:51 +0800

    corrected lint mistakes

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,7 +1,7 @@
 export default function News(){
   return (
-    <div className="container mx-auto px-4 py-8 mb-4">
-      "this is news page"
+    <div className='container mx-auto px-4 py-8 mb-4'>
+      this is news page
     </div>
   )
 }
